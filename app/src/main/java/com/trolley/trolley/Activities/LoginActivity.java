ca2da92b4c3d305df430cbb18ffb9885c271307c
rahulpandey261 <rahulpandey261@gmail.com>
2015-09-25 20:15:33 +0530

    some chnages

--- app/src/main/java/com/trolley/trolley/Activities/LoginActivity.java
+++ app/src/main/java/com/trolley/trolley/Activities/LoginActivity.java
@@ -1,10 +1,15 @@
 package com.trolley.trolley.Activities;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,20 +17,49 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.trolley.trolley.Application.TrolleyApp;
+import com.trolley.trolley.Constants.TrolleyConstants;
 import com.trolley.trolley.Fragments.TrolleyDialogs;
 import com.trolley.trolley.R;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener, TrolleyDialogs.TrolleyDialogsListener {
     Button mSignUp, mSkip;
     ImageButton mSignIn;
+    boolean islogin = false;
+    private Toolbar toolbar;
+    private TextInputLayout inputUsername;
+    private TextInputLayout inputPassword;
+    private ProgressDialog pDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        //toolbar.setLogo(android.R.drawable.ic_menu_close_clear_cancel);
+        toolbar.setTitle("Login");
+        setSupportActionBar(toolbar);
+
         mSignIn = (ImageButton) findViewById(R.id.login);
         mSignUp = (Button) findViewById(R.id.sign_up);
         mSkip = (Button) findViewById(R.id.skip);
+
+        inputUsername = (TextInputLayout) findViewById(R.id.usernameWrapper);
+        inputPassword = (TextInputLayout) findViewById(R.id.passwordWrapper);
+        // Progress dialog
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+
         mSkip.setOnClickListener(this);
         mSignUp.setOnClickListener(this);
         mSignIn.setOnClickListener(this);
@@ -36,7 +70,17 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         switch (v.getId()) {
             case R.id.login:
-                Toast.makeText(LoginActivity.this, "Login", Toast.LENGTH_SHORT).show();
+               // Toast.makeText(LoginActivity.this, "Login", Toast.LENGTH_SHORT).show();
+                String username = inputUsername.getEditText().getText().toString();
+                String password = inputPassword.getEditText().getText().toString();
+                // Check for empty data in the form
+                if (username.trim().length() > 0 && password.trim().length() > 0) {
+                    // login user
+                    checkLogin(username, password);
+                } else {
+                    // Prompt user to enter credentials
+                    Toast.makeText(getApplicationContext(), "Please enter the credentials!", Toast.LENGTH_LONG).show();
+                }
                 break;
             case R.id.sign_up:
                 Toast.makeText(LoginActivity.this, "sign up", Toast.LENGTH_SHORT).show();
@@ -49,11 +93,73 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     }
 
+
     private void showSignUpDialog() {
-        TrolleyDialogs trolleyDialogs = new TrolleyDialogs();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-        transaction.add(android.R.id.content, trolleyDialogs).addToBackStack(null).commit();
+        DialogFragment newFragment = new TrolleyDialogs();
+        newFragment.show(getSupportFragmentManager(), "login");
+
+    }
+
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        Dialog dialogView = dialog.getDialog();
+        TextInputLayout textInputLayout = (TextInputLayout) dialogView.findViewById(R.id.usernameWrapper);
+        Toast.makeText(LoginActivity.this, textInputLayout.getEditText().getText().toString(), Toast.LENGTH_SHORT).show();
+        dialog.dismiss();
+    }
+
+    private void showDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
+    }
+
+
+    //login json request
+    private void checkLogin(final String username, final String password) {
+        String tag_string_req = "req_login";
+        pDialog.setMessage("Logging in ...");
+        showDialog();
+        StringRequest strReq = new StringRequest(Request.Method.POST, TrolleyConstants.sWebUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                hideDialog();
+                JSONObject jObj = null;
+                try {
+                    jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+                    if (!error) {
+                        Toast.makeText(LoginActivity.this, jObj.getString("user_location"), Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(LoginActivity.this, jObj.getString("message"), Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Log.e("Rahul", "Login Error: " + volleyError.getMessage());
+                Toast.makeText(getApplicationContext(), volleyError.getMessage(), Toast.LENGTH_LONG).show();
+                hideDialog();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting parameters to login url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("tag", "login");
+                params.put("user_name", username);
+                params.put("user_password", password);
+                return params;
+            }
+        };
+        TrolleyApp.getInstance().addToReqQueue(strReq);
     }
 }
--- app/src/main/java/com/trolley/trolley/Fragments/TrolleyDialogs.java
+++ app/src/main/java/com/trolley/trolley/Fragments/TrolleyDialogs.java
@@ -1,11 +1,13 @@
 package com.trolley.trolley.Fragments;
 
-
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.trolley.trolley.R;
 
@@ -13,10 +15,52 @@ import com.trolley.trolley.R;
  * Created by RAHUL on 24-Sep-15.
  */
 public class TrolleyDialogs extends DialogFragment {
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout to use as dialog or embedded fragment
-        return inflater.inflate(R.layout.trolley_dialog, container, false);
+
+
+    private TrolleyDialogsListener mListener;
+    Toolbar mToolbar;
+
+    public TrolleyDialogs() {
+        // Empty constructor required for DialogFragment
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setCancelable(false);
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Build the dialog and set up the button click handlers
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setView(inflater.inflate(R.layout.trolley_dialog, null))
+                .setPositiveButton(R.string.positive_btn, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Send the positive button event back to the host activity
+                        mListener.onDialogPositiveClick(TrolleyDialogs.this);
+                    }
+                });
+        return builder.create();
+    }
+
+    public interface TrolleyDialogsListener {
+        public void onDialogPositiveClick(DialogFragment dialog);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            mListener = (TrolleyDialogsListener) activity;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(activity.toString()
+                    + " must implement NoticeDialogListener");
+        }
+    }
+
 }
